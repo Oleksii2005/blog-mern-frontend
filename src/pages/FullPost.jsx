8d32067b432c14f1f3af15c3ef79103104fec028
Oleksii2005 <122476976+Oleksii2005@users.noreply.github.com>
2024-01-19 23:35:08 +0100

    posts and image request

--- src/pages/FullPost.jsx
+++ src/pages/FullPost.jsx
@@ -4,6 +4,7 @@ import { Post } from "../components/Post";
 import { Index } from "../components/AddComment";
 import { CommentsBlock } from "../components/CommentsBlock";
 import axios from "../axios";
+import ReactMarkdown from "react-markdown";
 
 export const FullPost = () => {
   const [data, setData] = React.useState();
@@ -30,7 +31,7 @@ export const FullPost = () => {
       <Post
         id={data._id}
         title={data.title}
-        imageUrl={data.imageUrl}
+        imageUrl={`http://localhost:4444${data.imageURL}`}
         user={data.user}
         createdAt={data.createdAt}
         viewsCount={data.viewsCount}
@@ -38,7 +39,8 @@ export const FullPost = () => {
         tags={data.tags}
         isFullPost
       >
-        <p>{data.text}</p>
+        <ReactMarkdown children={data.text} />
+        {/* <p>{data.text}</p> */}
       </Post>
       <CommentsBlock
         items={[
